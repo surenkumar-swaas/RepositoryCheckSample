9028ac2268b30fc8d5d742b2da8e7cac42593eb3
surenkumar03swaas <surenkumar@swaas.net>
2021-12-28 15:06:49 +0530

    Add the comment on home controller

--- RepositoryCheckSample/Controllers/HomeController.cs
+++ RepositoryCheckSample/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace RepositoryCheckSample.Controllers
 {
     public class HomeController : Controller
     {
+        //home controller
         public ActionResult Index()
         {
             return View();
